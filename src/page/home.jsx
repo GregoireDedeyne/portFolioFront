1b1e756f99fdf57f6a4569d26486dad4160a9f43
gregoireDedeyne <gregoire.dedeyne@gmail.com>
2024-04-14 18:01:28 +0200

    update for

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -8,13 +8,13 @@ export function Home() {
 				<Header />
 			</div>
 			<div className="bg-fond-one bg-cover h-screen">
-				<p> hello </p>
+				<p> Présentation </p>
 			</div>
 			<div className="bg-fond-two bg-cover h-screen">
-				<p> hello </p>
+				<p> FORMATIONS </p>
 			</div>
 			<div className="bg-fond-three bg-cover h-screen">
-				<p> hello </p>
+				<p> SKILLS </p>
 			</div>
 			<div className="">
 				{" "}
