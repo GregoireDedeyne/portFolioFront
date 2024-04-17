a0524f89e1e29fdbc713973eea03fef78b12b6d6
gregoireDedeyne <gregoire.dedeyne@gmail.com>
2024-04-17 18:00:29 +0200

    adjust techno card

--- src/components/Techno/index.jsx
+++ src/components/Techno/index.jsx
@@ -7,14 +7,14 @@ export function Skills({ imgUrl, title, imgUrlBlack }) {
 			className="bg-theme-secondary dark:bg-theme-white w-fit h-14 flex justify-between rounded-3xl p-2"
 		>
 			<img
-				className="w-7 sm:w-10 lg:w-11 xl:w-auto object-contain hidden dark:block"
+				className="w-6 sm:w-10 lg:w-11 xl:w-auto object-contain hidden dark:block"
 				src={imgUrl}
 			/>
 			<img
-				className="w-7 sm:w-10 lg:w-11 xl:w-auto object-contain block dark:hidden"
+				className="w-6 sm:w-10 lg:w-11 xl:w-auto object-contain block dark:hidden"
 				src={imgUrlBlack}
 			/>
-			<p className="text-xs sm:text-base md:text-lg lg:text-xl xl:text-2xl font-medium dark:text-black text-white  pr-1">
+			<p className="text-[16px] flex items-center sm:text-base md:text-lg lg:text-xl xl:text-2xl font-medium dark:text-black text-white  pr-1">
 				{" "}
 				{title}{" "}
 			</p>
